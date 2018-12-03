285993e72f9d478b980ce3be2dc355cfb3f4ca19
Bhavesh Shrimali <bhavesh.shrimali@gmail.com>
2018-12-03 11:44:43 -0600

    Add files via upload

--- 2dfd.py
+++ 2dfd.py
@@ -23,12 +23,12 @@ from matplotlib.ticker import MaxNLocator
 from scipy.optimize import fsolve,minimize
 import pandas as pd
 import matplotlib.tri as mptri
-from matplotlib.ticker import AutoMinorLocator 
-import scipy.sparse as sp
+from matplotlib.ticker import AutoMinorLocator,LogLocator 
 from scipy.interpolate import interp1d,splrep,splder,splev
 from scipy.integrate import solve_bvp  #verify the FEM solution
 from scipy.interpolate import griddata
 from sympy import Symbol,diff,lambdify,log,transpose,Matrix,flatten,Array,tensorproduct
+import pyamg as pmg
 #from sympy import Symbol,diff,Array,lambdify,tensorproduct,Matrix,flatten
 
 ##################################################################
@@ -38,15 +38,21 @@ matplotlib.rcParams['xtick.direction']='in'
 matplotlib.rcParams['ytick.direction']='in'
 matplotlib.rcParams['xtick.top']=True
 matplotlib.rcParams['ytick.right']=True
-rc('xtick',labelsize=18)
-rc('ytick',labelsize=18)
+rc('xtick',labelsize=22)
+rc('ytick',labelsize=22)
 matplotlib.rcParams['xtick.major.pad']=10
-mx=AutoMinorLocator(10)
-my=AutoMinorLocator(10)
+sptsy=np.array([i for i in np.linspace(0.1,1,10)])
+sptsx=np.array([i for i in np.linspace(0.1,1,10)])
+
+xmintick=AutoMinorLocator(20)
+ymintick=AutoMinorLocator(20)
+ymintickLog=LogLocator(base=10.0,subs=tuple(sptsy),numticks=len(sptsy)+1)
+xmintickLog=LogLocator(base=10.0,subs=tuple(sptsx),numticks=len(sptsx)+1)
+
 ##################################################################
 class geometry() : #1D geometry
     def __init__(self,Eltype):
-        self.tolNR=1.e-6
+        self.tolNR=1.e-8
         if Eltype[0]=='L':
             self.A=8.e-1
             self.B=1.
@@ -59,10 +65,10 @@ class geometry() : #1D geometry
             self.Po=2.5e5*np.linspace(0,1,100)
             self.epS=2.*np.linspace(1,20,100)
         elif Eltype[0]=='Q':
-            self.xlength=2
-            self.ylength=2
-            self.nx=1
-            self.ny=1
+            self.xlength=1
+            self.ylength=1
+            self.nx=25
+            self.ny=25
             nel = (self.nx-1)*(self.ny-1)
             self.NE = nel
             self.nDim=2                                                        #No. of dof per node, it is essentially the dimension of the problem
@@ -72,7 +78,7 @@ class geometry() : #1D geometry
             kap=1.e1*self.mu 
             self.lam=40.
 #            self.tolNR=1.e-10
-            self.maxiter=100
+            self.maxiter=50000
             
 
 def meshgenerate(order):
@@ -165,7 +171,7 @@ class basis():  # defined on the canonical element (1D : [-1,1], 2D (Q): [-1,1] 
 class DWDIi():               
     def __init__(self,ndim):
         I1=Symbol('I1');J=Symbol('J');
-        W = 1/2.*geom.mu*(I1-2.)-geom.mu*log(J)+geom.lam/2.*(J-1.)**2               #change W here to include the modified Neo-Hookean
+        W = 1/2.*geom.mu*(I1-2.)-geom.mu*(J-1.)+geom.lam/2.*(J-1.)**2               #change W here to include the modified Neo-Hookean
         dWdI1=W.diff(I1,1)
         dWdJ=W.diff(J,1)
 #        print(dWdJ)
@@ -209,38 +215,49 @@ def locmat(nodes,de):                     #local stiffness (jacobian) and force 
 #    print(gDshpL[:,:,2])
     Je=np.einsum('ilk,lj->ijk',gDshpL,nodes.reshape(geom.nDim,-1).T)                  # computing the jacobian
 #    print(Je[:,:,0])
-    detJ=np.dstack(la.det(Je[:,:,i]) for i in range(Xi.shape[0]))        # 1x1xNgP                  # try making it faster by removing the generator
-    Jeinv=np.dstack(la.inv(Je[:,:,i]) for i in range(Xi.shape[0]))       # 3x3xNgP       #avoid computing inverse on a loop (--check ?)
+#    detJ=np.dstack(la.det(Je[:,:,i]) for i in range(Xi.shape[0]))        # 1x1xNgP                  # try making it faster by removing the generator
+    detJ=(Je[0,0,:]*Je[1,1,:]-Je[0,1,:]*Je[1,0,:])
+    Jeinv=1/detJ*np.array([[Je[1,1,:],-Je[0,1,:]],[-Je[1,0,:],Je[0,0,:]]])
+#    Jeinv=np.dstack(la.inv(Je[:,:,i]) for i in range(Xi.shape[0]))       # 2x2xNgP       #avoid computing inverse on a loop (--check ?)
     gDshpG=np.einsum('ilk,ljk->ijk',Jeinv,gDshpL)                                     #global derivatives  (remains the same, even for 3D ? )
     Bmat=np.kron(np.eye(geom.nDim).reshape(geom.nDim,geom.nDim,-1),gDshpG)            
     gradU=np.einsum('ilk,ljk->ijk',Bmat,dofloc)                         # 9x1xNgP                   #remember that gradU is never symmetric !!!
-#     print(gradU[:,:,0])
+#    print(gradU[:,:,0])
 #    print(gDshpG.shape)
     """
     Computing the deformation gradient (F12,F11,F22).T = B*de, and first piola (S) --> (S12,S11,S22), 
     Multiplying by the Gauss-weights, and calculating the element residual
     """
-    F=gradU+np.eye(geom.nDim).reshape(-1,1,1).repeat(Xi.shape[0],axis=-1)      #convert to 3x3xNgP and the take det(F) and inv(F)
-    F2b2 = F.reshape(geom.nDim,geom.nDim,-1)
-    detF = np.dstack(la.det(F2b2[:,:,i] )for i in range(Xi.shape[0]))
-    Finv2b2 = np.dstack(la.inv(F2b2[:,:,i] )for i in range(Xi.shape[0]))
-    Finv2b2T = np.einsum('ijk->jik',Finv2b2)
-    FinvT = Finv2b2T.reshape(-1,1,Xi.shape[0])
-    Finv = Finv2b2.reshape(-1,1,Xi.shape[0])
+    F=gradU+np.eye(geom.nDim).reshape(-1,1,1).repeat(Xi.shape[0],axis=-1)      #convert to 2x2xNgP and the take det(F) and inv(F)
+#    F2b2 = F.reshape(geom.nDim,geom.nDim,-1)
+    detF=F[0]*F[3]-F[1]*F[2] 
+#    detF = np.dstack(la.det(F2b2[:,:,i] ) for i in range(Xi.shape[0]))
+#    Finv2b2 = np.dstack(la.inv(F2b2[:,:,i] ) for i in range(Xi.shape[0]))
+#    Finv2b2T = np.einsum('jik',Finv2b2)
+#    FinvT = Finv2b2T.reshape(-1,1,Xi.shape[0])
+#    Finv = Finv2b2.reshape(-1,1,Xi.shape[0])
+    Finv=np.array([F[3],-F[1],-F[2],F[0]])/detF
+    FinvT = Finv[np.array([0,2,1,3],int)]
     #    print(gradU[:,:,0])
 #    detF = F[0]*F[3]-F[1]*F[2]
 #    detF=(F[0]*F[3]-F[1]*F[2]).reshape(1,1,-1)#.repeat(Xi.shape[0],axis=-1)   
-    WpI1=dWdIi.DWDI1(*F).squeeze().reshape(1,1,-1)#.repeat(Xi.shape[0],axis=-1)
-    WpJ=dWdIi.DWDJ(*F).squeeze().reshape(1,1,-1)#.repeat(Xi.shape[0],axis=-1)
-    WppI1=dWdIi.D2WDI12(*F).squeeze().reshape(1,1,-1)#.repeat(Xi.shape[0],axis=-1)
-    WppJ=dWdIi.D2WDJ2(*F).squeeze().reshape(1,1,-1)#.repeat(Xi.shape[0],axis=-1)
+    WpI1=dWdIi.DWDI1(*F).squeeze().reshape(-1,1,1)#.repeat(Xi.shape[0],axis=-1)
+    WpJ=dWdIi.DWDJ(*F).squeeze().reshape(-1,1,1)#.repeat(Xi.shape[0],axis=-1)
+    WppI1=dWdIi.D2WDI12(*F).squeeze().reshape(-1,1,1)#.repeat(Xi.shape[0],axis=-1)
+    WppJ=dWdIi.D2WDJ2(*F).squeeze().reshape(-1,1,1)#.repeat(Xi.shape[0],axis=-1)
+    WpI1 = np.einsum('kji',WpI1)
+    WpJ = np.einsum('kji',WpJ)
+    WppI1 = np.einsum('kji',WppI1)
+    WppJ = np.einsum('kji',WppJ)
+    
+    
 #    Finv=np.array([F[3],-F[1],-F[2],F[0]])/detF                                 # avoid computing inverse on the loop for the deformation gradient    
 #    FinvT = np.array([F[3],-F[2],-F[1],F[0]])/detF
 #    print('shape = ',np.array([F[3],-F[2],-F[1],F[0]]).shape)
 #    Helpful variables:     
     S=WpI1*2.*F+(WpJ*detF)*FinvT         # notice the swap of axes for transpose
     fac=Wg*detJ*geom.thck
-    S*=fac                                                                      # multiplying S by the determinant of the jacobian, thickness, and gauss-weights
+    S *= fac                                                                      # multiplying S by the determinant of the jacobian, thickness, and gauss-weights
     res=np.einsum('lik,ljk->ij',Bmat,S)                                         # double contraction along axis 1 and 2 (of B)
     """
     Computing the Consistent Tangent:D= B^T *C *B    <-- Cijkl, check notes
@@ -277,17 +294,14 @@ def locmat(nodes,de):                     #local stiffness (jacobian) and force 
                 [C1121,C1221,C2121,C2122],
                 [C1122,C1222,C2122,C2222]])
     
-    D=np.einsum('lik,lpk,pjk->ij',Bmat,C,Bmat)                                  #Check the multiplication once for a simple case!
+    D=np.einsum('lik,lpk,pjk->ij',Bmat,C*fac,Bmat)                                  #Check the multiplication once for a simple case!
     IntptGlob = np.einsum('ilj,l->ij',Nshp,nodes)
     return D,res.flatten(),S,F,IntptGlob,Xi.shape[0]
 
 def assembly(disp):
-    globK=0.*np.eye(disp.size)
+    globK=sp.lil_matrix(1.e-15*np.eye(disp.size))
     globF=np.zeros(disp.size)
-#    strs = np.zeros((geom.NE,2,2,NGPts))
-#    DG = np.zeros((geom.NE,2,2,NGPts))
-#    intpt = strs.copy()
-    
+
     for i in range(conVxy.shape[0]):
         elnodes=conVxy[i]
         globdof=np.array([2*elnodes,2*elnodes+1]).flatten()#.T.flatten()
@@ -302,6 +316,7 @@ def assembly(disp):
         Strn=(np.einsum('lik,ljk->ijk',DG.reshape(geom.nDim,geom.nDim,-1),DG.reshape(geom.nDim,geom.nDim,-1))-np.eye(geom.nDim).reshape(geom.nDim,geom.nDim,-1).repeat(ngp,axis=-1))/2
     strs = strs.reshape(geom.nDim,geom.nDim,-1)
     DG = DG.reshape(geom.nDim,geom.nDim,-1)
+    globK = globK.tocsr().copy()
     return globK,globF,strs,DG,Strn,intpt
 
 Eltype='Q1'
@@ -315,7 +330,7 @@ dWdIi=DWDIi(geom.nDim)
 meshxy=meshgenerate(1)['msh']
 conVxy=meshgenerate(1)['connv']
 identify_nodeBC = node_sets_bc(meshxy,conVxy)
-dof=1.e9*np.ones(meshxy.size) #initializing dofs (displacement of nodes)
+dof=-20*np.ones(meshxy.size) #initializing dofs (displacement of nodes)
 intpt=([])
 
 
@@ -327,7 +342,7 @@ dofs_bottom_right_y = 2*identify_nodeBC.nodes_bottom_idx+1
 dofs_bottom = np.hstack((dofs_bottom_left_x,dofs_bottom_right_y)) 
 pres_dofs = np.hstack((dofs_top_y,dofs_bottom)).T
 num_pres_dof = pres_dofs.size 
-dofs_top_yval = .0005*geom.ylength*np.ones(dofs_top_y.size)
+dofs_top_yval = 0.55*geom.ylength*np.ones(dofs_top_y.size)
 pres_dofs_top = np.vstack((dofs_top_y,dofs_top_yval)).T
 pres_dofs_bottom = np.vstack((dofs_bottom,0*dofs_bottom)).T
 prescribed_dofs = np.vstack((pres_dofs_top,pres_dofs_bottom))
@@ -335,17 +350,19 @@ prescribed_dofs = np.vstack((pres_dofs_top,pres_dofs_bottom))
 lineardof = np.zeros(dof.size)
 lineardof[(prescribed_dofs[:,0]).astype(int)]=prescribed_dofs[:,1]
 dof[(prescribed_dofs[:,0]).astype(int)]=0
-fdof=dof==1.e9                          #free dofs flags: further initialization to zeros needed only for the first step 
+fdof=dof==-20.                       #free dofs flags: further initialization to zeros needed only for the first step 
 nfdof=np.invert(fdof)                   #fixed dofs flags
 dof[fdof]=0.
 
 #Collect the linear stiffness / force - vector for reference to solve a linear problem
 Ks,Fs,_,_,_,Gauss_pt_global = assembly(lineardof)
-lineardof[fdof]=la.solve(Ks[np.ix_(fdof,fdof)],-Ks[np.ix_(fdof,nfdof)] @ lineardof[nfdof])
+lineardof[fdof]=sla.spsolve(Ks[np.ix_(fdof,fdof)],-Ks[np.ix_(fdof,nfdof)] @ lineardof[nfdof])
 Gauss_pt_global=Gauss_pt_global[0].T
 dofstore=np.zeros(dof.shape)
 
 DfGrn=([]);Strs=([]);LagStrain=([])
+residualStep = []
+flag=True
 for i in range(geom.nSteps):
     print('Step: ',i)
     dof[(prescribed_dofs[:,0]).astype(int)]=(i+1)/(geom.nSteps)*prescribed_dofs[:,1]
@@ -353,20 +370,58 @@ for i in range(geom.nSteps):
     normres0=la.norm(Fs1[fdof],2)
     normres = normres0.copy()
     iterNR=0
-    while normres >= geom.tolNR:#* normres0:# and iterNR <= geom.maxiter: 
-        print('Iter: {}'.format(iterNR))
-        del_dof =  la.solve(Ks1[np.ix_(fdof,fdof)],-Fs1[fdof])                #external force add (-- not required here, only for this case though)
+    resNewton = []
+    while normres >= geom.tolNR* normres0 and iterNR <= geom.maxiter: 
+#        print('Iter: {}'.format(iterNR))
+        res=[]
+        Bvec = np.ones((Ks1[np.ix_(fdof,fdof)].shape[0],1))
+        smooth=('energy', {'krylov': 'cg', 'maxiter': 50, 'degree': 8, 'weighting': 'local'})
+        ml = pmg.smoothed_aggregation_solver(Ks1[np.ix_(fdof,fdof)], Bvec, strength='evolution', max_coarse=50,
+                                       smooth=smooth)
+        
+#        ml = pmg.smoothed_aggregation_solver(Ks1[np.ix_(fdof,fdof)],max_levels=20)
+        del_dof = ml.solve(-Fs1[fdof],-Fs1[fdof],tol=1.e-9,accel='cg',residuals=res)        #np.random.random(Ks1[np.ix_(fdof,fdof)].shape[0])
+#        del_dof =  sla.spsolve(Ks1[np.ix_(fdof,fdof)],-Fs1[fdof])                #external force add (-- not required here, only for this case though)
         dof[fdof] += del_dof.copy() 
         Ks1,Fs1,strs,DG,Es,_ = assembly(dof)
         normres=la.norm(Fs1[fdof],2)
+        print('Residual Norm: {}   Iter: {}   LoadStep: {}'.format(normres,iterNR+1,i+1))
+        if normres >= 1.e3:
+            break
+        resNewton.append(res)
 #        print(la.norm(Fs1[fdof]))
         iterNR += 1
+    if normres >= 1.e3:
+        flag==False
+        break
+        
     Strs.append(strs)
     DfGrn.append(DG)
     LagStrain.append(Es)
     dofstore=np.vstack((dofstore,dof))
+    residualStep.append(resNewton)
 dofstore = dofstore.T
 DfGrn=np.array(DfGrn);LagStrain=np.array(LagStrain);Strs=np.array(Strs)
+if flag:
+    for idx,resx in enumerate(residualStep):
+        fig,axglob = plt.subplots(1,1,figsize=(8,8))
+        axglob.set_title(r'Load Step: ${0:1d}$'.format(idx+1),fontsize=22)
+        for idxNewton,res_newton in enumerate(resx):
+            axglob.semilogy(res_newton,'-o',label=r'Newton iteration: ${0:1d}$'.format(idxNewton+1))
+        axglob.set_xlabel(r'Iterations',fontsize=22)
+        axglob.set_ylabel(r'Residual Norm',fontsize=22)    
+        axglob.legend(loc=0,fontsize=22)
+        axglob.grid(True,linestyle='--')
+        axglob.yaxis.set_minor_locator(ymintickLog)
+        axglob.xaxis.set_minor_locator(xmintick)
+        fig.tight_layout()
+#    fig.savefig('Step'+str(idx+1)+'.png',dpi=400)
+#    plt.close()
+# Post processing using VTK
+#import vtk 
+
+
+
 #plt.figure(figsize=(8,8))
-#plt.tricontourf(meshxy[:,0],meshxy[:,1],dof[np.arange(1,dof.size,2)])
+#plt.tricontourf(meshxy[:,0],meshxy[:,1],dof[np.arange(1,dof.size,2)],cmap=plt.cm.get_cmap('RdBu'))
 #plt.colorbar()
